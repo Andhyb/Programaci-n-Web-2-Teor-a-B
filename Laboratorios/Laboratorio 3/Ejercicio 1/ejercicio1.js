dda5ba12c5bb5e87c3908c34515d563e527ef9c7
Andhyb <achipanamam@unsa.edu.pe>
2025-04-23 12:08:45 -0500

    EJERCICIO 1: agregar la función obtener dia

--- Laboratorios/Laboratorio 3/Ejercicio 1/ejercicio1.js
+++ Laboratorios/Laboratorio 3/Ejercicio 1/ejercicio1.js
@@ -1,1 +1,9 @@
 
+function obtenerDia(numeroDia) {
+    const diasSemana = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
+    if (numreroDia >= 0 && numeroDia <= 6) {
+        return diasSemana[numeroDia];
+    } else {
+        return "Número de día inválido";
+    }
+}
