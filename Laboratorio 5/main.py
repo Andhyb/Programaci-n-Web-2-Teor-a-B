dc5f40ef950a01481114c78b989dea88bd113ff0
Andhy Chipana Mamani <achipanamam@unsa.edu.pe>
2025-05-11 20:02:20 -0500

    Cambio el main para probar la imagen c usando horizontalRepeat, dandole un valor de 4 a n para que la imagen de la reina se repita 4 veces

--- Laboratorio 5/main.py
+++ Laboratorio 5/main.py
@@ -1,17 +1,10 @@
 from Tarea_del_Ajedrez import chessPictures, interpreter
 
-# Obtener caballo blanco
-white_knight = chessPictures.knight
-# Crear caballo negro aplicando negativo
-black_knight = white_knight.negative()
+# Obtenemos la reina blanca
+queen = chessPictures.queen
 
-# Construir las dos filas
-fila1 = white_knight.join(black_knight)
-fila2 = white_knight.verticalMirror().join(black_knight.verticalMirror())
+# La repetimos 4 veces
+resultado = queen.horizontalRepeat(4)
 
-# Unir arriba y abajo
-resultado = fila2.up(fila1)
-
-
-# Mostrar la imagen resultante
+# Mostramos la imagen
 interpreter.draw(resultado)
