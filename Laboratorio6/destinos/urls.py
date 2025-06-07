9ad05308b1dd9cdcdf29380316674d59ae3e29bc
Andhy Chipana Mamani <achipanamam@unsa.edu.pe>
2025-06-07 13:42:09 -0500

    Agreamos la vista en la pagina para editar destinos y ponemos su ruta en urls

--- Laboratorio6/destinos/urls.py
+++ Laboratorio6/destinos/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('destinos/', views.lista_destinos, name='lista_destinos'),
     path('destinos/agregar/', views.agregar_destino, name='agregar_destino'),
+    path('destinos/editar/<int:destino_id>/', views.editar_destino, name='editar_destino'),
 ]
