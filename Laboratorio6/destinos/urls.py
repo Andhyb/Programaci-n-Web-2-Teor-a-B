3d7cdb22ab711361ef1db7244ceb475685176f71
Andhy Chipana Mamani <achipanamam@unsa.edu.pe>
2025-06-07 15:52:15 -0500

    importamos las vistas de login y logout en urls

--- Laboratorio6/destinos/urls.py
+++ Laboratorio6/destinos/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('destinos/', views.lista_destinos, name='lista_destinos'),
@@ -7,4 +8,6 @@ urlpatterns = [
     path('destinos/editar/<int:destino_id>/', views.editar_destino, name='editar_destino'),
     path('destinos/eliminar/<int:destino_id>/', views.eliminar_destino, name='eliminar_destino'),
     path('registro/', views.registro, name='registro'),
+    path('login/', LoginView.as_view(template_name='destinos/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
 ]
