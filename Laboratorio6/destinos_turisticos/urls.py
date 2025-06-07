5adf2126d6b3122676bed3f8998e7a0cb93d7b48
Andhy Chipana Mamani <achipanamam@unsa.edu.pe>
2025-06-07 16:11:58 -0500

    Corregimos varios errores y cambiamos el html de lista de destinos por un error en el logout

--- Laboratorio6/destinos_turisticos/urls.py
+++ Laboratorio6/destinos_turisticos/urls.py
@@ -19,8 +19,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth.views import LogoutView
+from django.views.decorators.http import require_GET
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('destinos.urls')),
+    path('logout/', require_GET(LogoutView.as_view()), name='logout'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
