d80841cf41b6edaf43ba3edecd5cbe97b0692c5e
Andhy Chipana Mamani <achipanamam@unsa.edu.pe>
2025-06-06 21:34:24 -0500

    Vista agregar_destino implementada para crear destinos desde formulario

--- Laboratorio6/destinos/views.py
+++ Laboratorio6/destinos/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from django.shortcuts import render
 from .models import DestinoTuristico
+from .forms import DestinoForm
 
 def lista_destinos(request):
     destinos = DestinoTuristico.objects.all()
     return render(request, 'destinos/lista_destinos.html', {'destinos': destinos})
+
+def agregar_destino(request):
+    if request.method == 'POST':
+        form = DestinoForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_destinos')  # redirige al listado después de guardar
+    else:
+        form = DestinoForm()
+    return render(request, 'destinos/agregar_destino.html', {'form': form})
