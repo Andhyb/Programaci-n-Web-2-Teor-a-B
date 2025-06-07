078e0f41e4c4e1eb1d9cf63618930db23217ddb4
Andhy Chipana Mamani <achipanamam@unsa.edu.pe>
2025-06-07 14:27:14 -0500

    Agregamos el html para eliminar destinos y ponemos su ruta en urls

--- Laboratorio6/destinos/urls.py
+++ Laboratorio6/destinos/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('destinos/', views.lista_destinos, name='lista_destinos'),
     path('destinos/agregar/', views.agregar_destino, name='agregar_destino'),
     path('destinos/editar/<int:destino_id>/', views.editar_destino, name='editar_destino'),
+    path('destinos/eliminar/<int:destino_id>/', views.eliminar_destino, name='eliminar_destino'),
 ]
