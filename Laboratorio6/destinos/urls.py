52ee05038d22a84b85bb7031b9d423dd17818c52
Andhy Chipana Mamani <achipanamam@unsa.edu.pe>
2025-06-07 15:48:52 -0500

    Agregamos el html de registro y ponemos su ruta en urls

--- Laboratorio6/destinos/urls.py
+++ Laboratorio6/destinos/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('destinos/agregar/', views.agregar_destino, name='agregar_destino'),
     path('destinos/editar/<int:destino_id>/', views.editar_destino, name='editar_destino'),
     path('destinos/eliminar/<int:destino_id>/', views.eliminar_destino, name='eliminar_destino'),
+    path('registro/', views.registro, name='registro'),
 ]
