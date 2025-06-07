6ce93ac94e7b0768baf2ec8814082c92b29c23f8
Andhy Chipana Mamani <achipanamam@unsa.edu.pe>
2025-06-07 15:54:08 -0500

    Creamos el html para login y aplicamos el decorador a las vistas que solo deben funcuonar si el usuario está logueado en views.py

--- Laboratorio6/destinos/views.py
+++ Laboratorio6/destinos/views.py
@@ -2,15 +2,18 @@ from django.shortcuts import render, redirect
 from django.shortcuts import get_object_or_404
 from django.contrib.auth import login
 from .forms import RegistroUsuarioForm
+from django.contrib.auth.decorators import login_required
 
 from django.shortcuts import render
 from .models import DestinoTuristico
 from .forms import DestinoForm
 
+@login_required
 def lista_destinos(request):
     destinos = DestinoTuristico.objects.all()
     return render(request, 'destinos/lista_destinos.html', {'destinos': destinos})
 
+@login_required
 def agregar_destino(request):
     if request.method == 'POST':
         form = DestinoForm(request.POST, request.FILES)
@@ -21,6 +24,7 @@ def agregar_destino(request):
         form = DestinoForm()
     return render(request, 'destinos/agregar_destino.html', {'form': form})
 
+@login_required
 def editar_destino(request, destino_id):
     destino = get_object_or_404(DestinoTuristico, id=destino_id)
     if request.method == 'POST':
@@ -32,6 +36,7 @@ def editar_destino(request, destino_id):
         form = DestinoForm(instance=destino)
     return render(request, 'destinos/editar_destino.html', {'form': form})
 
+@login_required
 def eliminar_destino(request, destino_id):
     destino = get_object_or_404(DestinoTuristico, id=destino_id)
     if request.method == 'POST':
